47e4f5a448f73d0653ef6853ed32509127c39d6f
velip <anders_teehee@hotmail.no>
2021-05-31 11:48:33 +0200

    Update raspdl.sh
    Updated the options. 
    Now shows flags and usage when no flags is set.
    Also added a header and it's more cooler looking now.

--- raspdl.sh
+++ raspdl.sh
@@ -1,9 +1,11 @@
-pass=P@SSWORD
+#!/bin/bash
+RED='\033[0;31m'
+LIME='\e[92m'
+NC='\033[0m' # No color
+
+pass=PASSWORD
 user=USERNAME
 ip=0.0.0.0
-
-RED='\033[0;31m'
-NC='\033[0m'
 
 while getopts r:l: flag
 do
@@ -13,7 +15,27 @@ do
     esac
 done
 
-sshpass -p $pass scp -- $user@$ip:$remote $local
-if [ -f "$local" ]; then
-  echo -e "[${RED}*${NC}] Downloaded '$remote' from '${RED}$ip${NC}' to '$local'";
+if [[ $* == *-r* ]] && [[ $* == *-l* ]]
+then
+  sshpass -p $pass scp -- $user@$ip:$remote $local
+  if [ -f "$local" ]; then
+    echo -e "[${RED}*${NC}] Downloaded '${LIME}$remote${NC}' from '${LIME}$user@$ip${NC}' to '${LIME}$local${NC}'";
+  fi
+else
+  echo -e "${LIME}
+                                 .___.__
+____________    ____________   __| _/|  |
+\_  __ \__  \  /  ___/\____ \ / __ | |  |
+ |  | \// __ \_\___ \ |  |_> > /_/ | |  |__
+ |__|  (____  /____  >|   __/\____ | |____/
+            \/     \/ |__|        \/ ${NC}
+
+  - By ${RED}www.github.com/v3lip${NC}
+  "
+  echo "Options:";
+  echo "    -r     remote path";
+  echo "    -l     local path";
+  echo "";
+  echo "Example: ./raspdl.sh -r '/home/username/Downloads/hello.txt' -l 'hello.txt'";
+  echo "";
 fi
